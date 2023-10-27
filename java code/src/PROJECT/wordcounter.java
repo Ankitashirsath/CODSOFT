c9596ca235213029cd9273ec624c6505173bc1b5
Ankitashirsath <135000632+Ankitashirsath@users.noreply.github.com>
2023-10-27 19:30:31 +0530

    initial commit

--- java code/src/PROJECT/wordcounter.java
+++ java code/src/PROJECT/wordcounter.java
@@ -1,10 +1,40 @@
 package PROJECT;
 
-public class wordcounter {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class wordcounter 
+{
+	public static void main(String[] args) 
+	{
+			Scanner in= new Scanner(System.in);
+			System.out.println("Enter a sentence");
+			String str = in.nextLine();
+			System.out.println("Enter a word");
+			String word = in.nextLine();
+			str += " ";
+			String word1="";
+			int count =0;
+			int len =str.length();
+			for(int i=0;i<len;i++)
+			{
+				if(str.charAt(i) == ' ')
+				{
+					if(word1.equalsIgnoreCase(word1))
+						count++;
+					word = "";
+				}
+				else
+				{
+					word += str.charAt(i);
+				}
+				}
+			if(count >0)
+			{
+				System.out.println(word + "is present"+count+"times.");
+			}
+			else
+			{
+				System.out.println(word +"is not presemt in sentence.");
+			}
+		}
 }
